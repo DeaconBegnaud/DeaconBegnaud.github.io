932242fc0f6d9c56c19b4a7aa67b4959d2453313
DeaconBegnaud <djb0200825@lpssonline.com>
2023-05-11 18:22:32 +0000

    add runtime project

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -19,6 +19,8 @@ var level01 = function (window) {
                 { "type": "sawblade", "x": 400, "y": groundY },
                 { "type": "sawblade", "x": 600, "y": groundY },
                 { "type": "sawblade", "x": 900, "y": groundY },
+                {"type": "enemy", "x": 700, "y": 70},
+                {"type": "reward", "x": 1000, "y": 70},
             ]
         };
         window.levelData = levelData;
@@ -39,11 +41,11 @@ sawBladeHitZone.addChild(obstacleImage);
 obstacleImage.x = -25
 obstacleImage.y = -25
         }
-        createSawBlade(750, 475)
+        createSawBlade(750, groundY)
         createSawBlade(1500 , 350)
-        createSawBlade(2000, 475)
-        createSawBlade(2200, 350)
-
+        createSawBlade(2000, groundY)
+        createSawBlade(2100, 350)
+        createSawBlade(2250, groundY)
         function createEnemy (x, y) {
             var enemy = game.createGameItem("enemy", 25);
 var redSquare = draw.rect(50, 50, "red");
@@ -53,20 +55,43 @@ enemy.addChild(redSquare);
 enemy.x = x;
 enemy.y = groundY - y;
 game.addGameItem(enemy)
-enemy.velocityX = -7
+enemy.velocityX = -2
 enemy.velocityY = 0
 enemy.onPlayerCollision = function () {
     game.changeIntegrity(-10)
 };
 enemy.onProjectileCollision = function () {
-    game.increaseScore(100);
+    game.increaseScore(200);
 enemy.shrink();
 }
         }
         createEnemy(700, 70)
         createEnemy (1300, 50)
         createEnemy(2000, 30)
-        
+        createEnemy(1200, 40)
+        createEnemy(1700, 60)
+        function createReward (x, y) {
+            var reward = game.createGameItem("reward", 25);
+var blueSquare = draw.rect(50, 50, "blue");
+blueSquare.x = -25;
+blueSquare.y = -25;
+reward.addChild(blueSquare);
+reward.x = x;
+reward.y = groundY - y;
+game.addGameItem(reward)
+reward.velocityX = -2
+reward.velocityY = 0
+reward.onPlayerCollision = function () {
+    game.changeIntegrity(10)
+};
+reward.onProjectileCollision = function () {
+    game.increaseScore(3000);
+reward.shrink();
+}
+        }
+        createReward(1000, 70)
+        createReward(2500, 50)
+       createReward(1500, 70)
         //work on this
         
         // DO NOT EDIT CODE BELOW HERE
