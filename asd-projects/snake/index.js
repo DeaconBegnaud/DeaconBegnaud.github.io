35af167937679484b2bb74790bebfcddcfdcef00
DeaconBegnaud <djb0200825@lpssonline.com>
2023-09-25 16:11:44 +0000

    saving data shapes

--- asd-projects/snake/index.js
+++ asd-projects/snake/index.js
@@ -47,8 +47,16 @@ init();
 
 function init() {
   // TODO 4b-2: initialize the apple
+  makeApple()
   // TODO 4c-2: initialize the snake
+  // initialize the snake's body as an empty Array
+ snake.body = [];
+
+ // make the first snakeSquare and set it as the head
+ makeSnakeSquare(10, 10);
+ snake.head = snake.body[0];
   // TODO 5a: Initialize the interval
+  updateInterval = setInterval(update, 100);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -61,6 +69,15 @@ function init() {
  */
 function update() {
   // TODO 5b: Fill in the update function's code block
+  moveSnake();
+
+    if (hasHitWall() || hasCollidedWithSnake()) {
+      endGame();
+    }
+
+    if (hasCollidedWithApple()) {
+      handleAppleCollision();
+    }
 }
 
 function checkForNewDirection(event) {
@@ -207,6 +224,27 @@ function makeApple() {
  */
 function makeSnakeSquare(row, column) {
   // TODO 4c-1: Fill in this function's code block
+  // initialize a new snakeSquare Object
+   var snakeSquare = {};
+
+   // make the snakeSquare.element Object and append it to the board
+  snakeSquare.element = $("<div>").addClass("snake").appendTo(board);
+
+   // initialize the row and column properties on the snakeSquare Object
+   snakeSquare.row = row;
+   snakeSquare.column = column;
+
+   // set the position of the snake on the screen
+   repositionSquare(snakeSquare);
+
+   // if this is the head, add the snake-head id
+   if (snake.body.length === 0) {
+     snakeSquare.element.attr("id", "snake-head");
+   }
+
+   // add snakeSquare to the end of the body Array and set it as the new tail
+   snake.body.push(snakeSquare);
+   snake.tail = snakeSquare;
 }
 
 /* 
